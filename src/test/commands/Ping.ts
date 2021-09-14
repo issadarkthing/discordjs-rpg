cf1b015a9227857d927135bcb6e8446d411f5f29
raziman <raziman.terra@gmail.com>
2021-09-14 21:36:20 +0800

    remove 'p' alias

--- src/test/commands/Ping.ts
+++ src/test/commands/Ping.ts
@@ -3,7 +3,6 @@ import { Command } from "@jiman24/commandment";
 
 export default class extends Command {
   name = "ping";
-  aliases = ["p"];
   throttle = 10 * 1000; // 10 seconds
 
   exec(msg: Message, args: string[]) {
